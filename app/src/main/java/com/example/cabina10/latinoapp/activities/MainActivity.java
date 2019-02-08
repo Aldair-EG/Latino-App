cd17c7878e0abbc0e1958f43cf9c8f96b88f4e2c
Marcelo <i1610110@continental.edu.pe>
2019-02-07 21:12:44 -0500

    Firebase

--- app/src/main/java/com/example/cabina10/latinoapp/activities/MainActivity.java
+++ app/src/main/java/com/example/cabina10/latinoapp/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cabina10.latinoapp.activities;
 
+
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -9,14 +10,29 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.cabina10.latinoapp.models.Student;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 import com.example.cabina10.latinoapp.R;
 import com.example.cabina10.latinoapp.adapters.DocumentAdapter;
 import com.example.cabina10.latinoapp.models.Document;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase datebaseFireBase;
+    DatabaseReference studentsReference;
+
+
 
     private ListView tvmain;
     @Override
@@ -41,6 +57,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(browserIntent);
             }
         });
+
+        datebaseFireBase = FirebaseDatabase.getInstance();
+        studentsReference = datebaseFireBase.getReference("students");
     }
 
     //Metodo para mostrar y ocultar Menu
@@ -59,8 +78,17 @@ public class MainActivity extends AppCompatActivity {
             Intent siguiente = new Intent(this, RegisterForm.class);
             startActivity(siguiente);
         }else if (id == R.id.item4){
-            Intent siguiente = new Intent(this, Map.class);
-            startActivity(siguiente);
+//            Intent siguiente = new Intent(this, Map.class);
+//            startActivity(siguiente);
+            Toast.makeText(this, studentsReference.getDatabase().toString(), Toast.LENGTH_SHORT).show();
+            ArrayList<Student> students = new ArrayList<>();
+
+            Student myNewStudent = new Student(1, 73275074,"123456","Marcelo","Rosales"
+            ,"Los sauces","i1610110@continental.edu.pe");
+            students.add(myNewStudent);
+
+            studentsReference.setValue(students);
+
         }else if (id == R.id.item3){
             finish();
         }
--- app/src/main/java/com/example/cabina10/latinoapp/models/Student.java
+++ app/src/main/java/com/example/cabina10/latinoapp/models/Student.java
@@ -1,25 +1,41 @@
 package com.example.cabina10.latinoapp.models;
-import java.sql.Struct;
 import java.util.Date;
 import java.util.ArrayList;
 
 public class Student {
-    int idStudent, DNI;
-    String Nombres, ApPaterno, ApMaterno, Domicilio, Correo, Telefono;
-    Date FechaNacimiento;
+    private int idStudent;
+    private int DNI;
+    private String Nombres;
+    private String ApMaterno;
+    private String ApPaterno;
+    private String Correo;
+    private String Domicilio;
+    private String Telefono;
+    private Date FechaNacimiento;
 
-    public Student(int _idStudent, int _DNI, String _Telefono, String _name, String _Paterno, String _Materno, String _Domicilio, String _Correo, Date _FechaNacimiento) {
-        this.idStudent = _idStudent;
-        this.DNI = _DNI;
-        this.Telefono = _Telefono;
-        this.Nombres = _name;
-        this.ApPaterno = _Paterno;
-        this.ApMaterno = _Materno;
-        this.Domicilio = _Domicilio;
-        this.Correo = _Correo;
-        this.FechaNacimiento = _FechaNacimiento;
+    public  Student(){
+
     }
-
+    public Student(int _idStudent, int _DNI, String _Telefono, String _name, String _Paterno, String _Domicilio, String _Correo) {
+        this.setIdStudent(_idStudent);
+        this.setDNI(_DNI);
+        this.setTelefono(_Telefono);
+        this.setNombres(_name);
+        this.setApPaterno(_Paterno);
+        this.setDomicilio(_Domicilio);
+        this.setCorreo(_Correo);
+    }
+    public Student(int _idStudent, int _DNI, String _Telefono, String _name, String _Paterno, String _Materno, String _Domicilio, String _Correo, Date _FechaNacimiento) {
+        this.setIdStudent(_idStudent);
+        this.setDNI(_DNI);
+        this.setTelefono(_Telefono);
+        this.setNombres(_name);
+        this.setApPaterno(_Paterno);
+        this.setApMaterno(_Materno);
+        this.setDomicilio(_Domicilio);
+        this.setCorreo(_Correo);
+        this.setFechaNacimiento(_FechaNacimiento);
+    }
     public String getDNI() { return String.format("%s", this.DNI); }
     public String getTelefono() { return this.Telefono; }
     public String getNombres() { return this.Nombres; }
@@ -27,7 +43,12 @@ public class Student {
     public String getApMaterno() { return this.ApMaterno; }
     public String getDomicilio() { return this.Domicilio; }
     public String getCorreo() { return this.Correo; }
-    public String getFechaNacimiento() { return this.FechaNacimiento.toString(); }
+    public String getFechaNacimiento() {
+        if (FechaNacimiento != null){
+        return this.FechaNacimiento.toString();}
+        else{
+            return "1990-03-03";
+        }};
 
     public static ArrayList<Student> getStudents() {
         ArrayList<Student> _list = new ArrayList<>();
@@ -36,5 +57,45 @@ public class Student {
         _list.add(new Student(001, 73568985, "956485232", "Alonso", "Campos", "Torres", "Jr. Los Lirios S/N", "acampos@gmail.com", new Date(2016,12,12)));
         _list.add(new Student(001, 73568985, "978562356", "Alonso", "Campos", "Torres", "Jr. Los Lirios S/N", "acampos@gmail.com", new Date(2016,12,12)));
         return _list;
+    }
+
+    public int getIdStudent() {
+        return idStudent;
+    }
+
+    public void setIdStudent(int idStudent) {
+        this.idStudent = idStudent;
+    }
+
+    public void setDNI(int DNI) {
+        this.DNI = DNI;
+    }
+
+    public void setNombres(String nombres) {
+        Nombres = nombres;
+    }
+
+    public void setApMaterno(String apMaterno) {
+        ApMaterno = apMaterno;
+    }
+
+    public void setApPaterno(String apPaterno) {
+        ApPaterno = apPaterno;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    public void setDomicilio(String domicilio) {
+        Domicilio = domicilio;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        FechaNacimiento = fechaNacimiento;
     }
 }
